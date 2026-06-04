a9f06ee26d3092a4f3f532d2a328f98d1159f8cb
ALEEM ULLAH <aleemsagar75@gmail.com>
2026-06-04 15:34:45 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 
-const cors = require("cors");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const { Pool } = require("pg");
+
+let cors = null;
+try {
+  cors = require("cors");
+} catch (_error) {
+  cors = null;
+}
 
 const app = express();
 const port = Number(process.env.PORT || 10000);
@@ -20,7 +26,19 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }
 });
 
-app.use(cors({ origin: true }));
+if (cors) {
+  app.use(cors({ origin: true }));
+} else {
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-sagarsoft-api-key, x-license-token");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(204);
+    }
+    return next();
+  });
+}
 app.use(express.json({ limit: "50mb" }));
 
 const webDirCandidates = [
