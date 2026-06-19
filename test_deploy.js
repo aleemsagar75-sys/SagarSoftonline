ee6dfdb9988e1843b4d2dc745588f28bdc4b0176
ALEEM ULLAH <aleemsagar75@gmail.com>
2026-06-19 15:13:47 +0500

    cleanup: remove old file test_deploy.js

--- test_deploy.js
+++
@@ -1,5 +0,0 @@
-// test marker - deployed at 2026-06-19 14:20:24
-const express = require('express');
-const app = express();
-app.get('/test-deploy', (req, res) => res.json({ deployed: true, time: '2026-06-19 14:20:24' }));
-app.listen(19999);
