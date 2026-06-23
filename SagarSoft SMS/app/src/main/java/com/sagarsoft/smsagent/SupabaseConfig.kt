1a08dd389257773d4fc534dee7e2aa214f32d0f1
ALEEM ULLAH <aleemsagar75@gmail.com>
2026-06-23 13:03:27 +0500

    Add Android SMS Agent project + SQL schema

---
+++ SagarSoft SMS/app/src/main/java/com/sagarsoft/smsagent/SupabaseConfig.kt
@@ -0,0 +1,6 @@
+package com.sagarsoft.smsagent
+
+object SupabaseConfig {
+    const val URL = "https://sbxgdvmjaapwhmnmqtfa.supabase.co"
+    const val ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNieGdkdm1qYWFwd2htbm1xdGZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NzA2NDMsImV4cCI6MjA5NDM0NjY0M30.Xe4Qxjk0RHHpwsbt-58VgIDnO3FaMpb4cD2euXf9IjI"
+}
