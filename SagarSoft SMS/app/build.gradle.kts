5f48a2e5ec886702a29af4e3b7f10fffb4a26080
ALEEM ULLAH <aleemsagar75@gmail.com>
2026-07-04 22:19:09 +0500

    Bump version to v2.4.0 (versionCode 14)

--- SagarSoft SMS/app/build.gradle.kts
+++ SagarSoft SMS/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sagarsoft.smsagent"
         minSdk = 24
         targetSdk = 34
-        versionCode = 13
-        versionName = "2.3.8"
+        versionCode = 14
+        versionName = "2.4.0"
     }
 
     buildTypes {
