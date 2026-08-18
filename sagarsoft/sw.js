0a67b85b24e6805798920eaad586e93ae4a40b61
ALEEM ULLAH <aleemsagar75@gmail.com>
2026-08-18 22:54:44 +0500

    v108 - timetable safety fixes, reorder UI, delete improvements, drag-drop UX

--- sagarsoft/sw.js
+++ sagarsoft/sw.js
@@ -1,10 +1,10 @@
-var CACHE = "sagarsoft-v106";
+var CACHE = "sagarsoft-v108";
 const PRECACHE_URLS = [
   "./",
   "./login.html",
   "./dashboard.html",
-  "./css/base.css?v=20260818a",
-  "./css/dashboard.css?v=20260818a",
+  "./css/base.css?v=20260818b",
+  "./css/dashboard.css?v=20260818b",
   "./js/online-config.js?v=20260729",
   "./js/crypto-utils.js?v=20260729",
   "./js/utils.js?v=20260729",
@@ -13,7 +13,7 @@ const PRECACHE_URLS = [
   "./js/auth.js?v=20260729",
   "./js/cache-manager.js?v=20260805",
   "./js/login.js?v=20260729",
-  "./js/dashboard.js?v=20260818a",
+  "./js/dashboard.js?v=20260818b",
   "./assets/SagarSoft.logo.png",
   "./assets/parents.png",
   "./manifest.json"
