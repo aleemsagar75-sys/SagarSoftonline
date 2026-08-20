12a4a0cf644da9fe3115103ff43e343f6c17eb86
ALEEM ULLAH <aleemsagar75@gmail.com>
2026-08-20 20:24:27 +0500

    v122: fix delete pointerup + drag move-only default

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "sagarsoft-v121";
+var CACHE = "sagarsoft-v122";
 const PRECACHE_URLS = [
   "./",
   "./login.html",
@@ -13,7 +13,7 @@ const PRECACHE_URLS = [
   "./js/auth.js?v=20260729",
   "./js/cache-manager.js?v=20260805",
   "./js/login.js?v=20260729",
-  "./js/dashboard.js?v=20260820f",
+  "./js/dashboard.js?v=20260820g",
   "./assets/SagarSoft.logo.png",
   "./assets/parents.png",
   "./manifest.json"
