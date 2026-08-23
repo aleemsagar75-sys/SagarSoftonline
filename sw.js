eb0ed5dc8a616660a6ad976646e931a708941df2
ALEEM ULLAH <aleemsagar75@gmail.com>
2026-08-23 21:45:17 +0500

    v132: Bump SW cache + precache URLs for fees report redesign

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-var CACHE = "sagarsoft-v131";
+var CACHE = "sagarsoft-v132";
 const PRECACHE_URLS = [
   "./",
   "./login.html",
   "./dashboard.html",
   "./css/base.css?v=20260823b",
-  "./css/dashboard.css?v=20260823b",
+  "./css/dashboard.css?v=20260823c",
   "./js/online-config.js?v=20260729",
   "./js/crypto-utils.js?v=20260729",
   "./js/utils.js?v=20260729",
@@ -13,7 +13,7 @@ const PRECACHE_URLS = [
   "./js/auth.js?v=20260729",
   "./js/cache-manager.js?v=20260805",
   "./js/login.js?v=20260729",
-  "./js/dashboard.js?v=20260823b",
+  "./js/dashboard.js?v=20260823c",
   "./assets/SagarSoft.logo.png",
   "./assets/parents.png",
   "./manifest.json"
