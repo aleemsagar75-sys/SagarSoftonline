d9464e6ba727d99ed3bbe4aa53a01e954bef93b4
ALEEM ULLAH <aleemsagar75@gmail.com>
2026-08-23 23:40:03 +0500

    v135: Class cards redesign - new CACHE, new ?v= URLs

--- sagarsoft/sw.js
+++ sagarsoft/sw.js
@@ -1,10 +1,10 @@
-var CACHE = "sagarsoft-v134";
+var CACHE = "sagarsoft-v135";
 const PRECACHE_URLS = [
   "./",
   "./login.html",
   "./dashboard.html",
   "./css/base.css?v=20260823b",
-  "./css/dashboard.css?v=20260824a",
+  "./css/dashboard.css?v=20260823f",
   "./js/online-config.js?v=20260729",
   "./js/crypto-utils.js?v=20260729",
   "./js/utils.js?v=20260729",
@@ -13,7 +13,7 @@ const PRECACHE_URLS = [
   "./js/auth.js?v=20260729",
   "./js/cache-manager.js?v=20260805",
   "./js/login.js?v=20260729",
-  "./js/dashboard.js?v=20260824a",
+  "./js/dashboard.js?v=20260823f",
   "./assets/SagarSoft.logo.png",
   "./assets/parents.png",
   "./manifest.json"
