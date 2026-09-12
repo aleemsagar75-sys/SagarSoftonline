d8eee205ff5db5ef9148044f35a431716a823688
ALEEM ULLAH <aleemsagar75@gmail.com>
2026-09-12 15:33:38 +0500

    fix: v150 - FCR data reading fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "sagarsoft-v149";
+var CACHE = "sagarsoft-v150";
 const PRECACHE_URLS = [
   "./",
   "./login.html",
@@ -13,7 +13,7 @@ const PRECACHE_URLS = [
   "./js/auth.js?v=20260729",
   "./js/cache-manager.js?v=20260805",
   "./js/login.js?v=20260729",
-  "./js/dashboard.js?v=20260911a",
+  "./js/dashboard.js?v=20260911b",
   "./assets/SagarSoft.logo.png",
   "./assets/parents.png",
   "./manifest.json"
