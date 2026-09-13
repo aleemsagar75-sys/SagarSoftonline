9aebeb3d473051981e9561813259a95d803c32fb
ALEEM ULLAH <aleemsagar75@gmail.com>
2026-09-13 16:29:30 +0500

    fix: FCR - only show paid/partial in table, recalc remaining, unpaid stats from all students (v159)

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-var CACHE = "sagarsoft-v162";
+var CACHE = "sagarsoft-v163";
 const PRECACHE_URLS = [
   "./",
   "./login.html",
   "./dashboard.html",
   "./css/base.css?v=20260823b",
-  "./css/dashboard.css?v=20260913c",
+  "./css/dashboard.css?v=20260913d",
   "./js/online-config.js?v=20260729",
   "./js/crypto-utils.js?v=20260729",
   "./js/utils.js?v=20260729",
@@ -13,7 +13,7 @@ const PRECACHE_URLS = [
   "./js/auth.js?v=20260729",
   "./js/cache-manager.js?v=20260805",
   "./js/login.js?v=20260729",
-  "./js/dashboard.js?v=20260913c",
+  "./js/dashboard.js?v=20260913d",
   "./assets/SagarSoft.logo.png",
   "./assets/parents.png",
   "./manifest.json"
